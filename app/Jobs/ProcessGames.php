5f78ebceb3d76d009aa206aa5dd544c90cb6ad1f
vikramIde <rapchik5845@gmail.com>
2018-11-18 20:37:58 +0530

    added

--- app/Jobs/ProcessGames.php
+++ app/Jobs/ProcessGames.php
@@ -7,24 +7,32 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Log;
+use Carbon\Carbon;
 
 class ProcessGames implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $gameId;
+    protected $game;
+    protected $team1;
+    protected $team2;
+    protected $score;
+
     protected $modelObj;
-    public    $result;
     protected $fieldObj;
+    protected $helperObj;
+    public    $result;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct($gameId)
+    public function __construct($game,$team1,$team2)
     {
-        //
-        $this->gameId = $gameId;
+        $this->game = $game;
+        $this->team1 = $team1;
+        $this->team2 = $team2;
     }
 
     /**
@@ -34,17 +42,31 @@ class ProcessGames implements ShouldQueue
      */
     public function handle()
     {
-        //
-        Log::info('Starting the Game for  ',['jobName'=>$this->gameId,'timeStamp'=>date("Y-m-d h:i:sa")]);
-        $this->modelObj = new \App\Game();
-        $this->fieldObj['conditions'] = ['id'=>$this->gameId,'status'=>1];
-        $this->result = $this->modelObj->fetchMap($this->fieldObj);
-        if($this->result['flag'] == 1){
-              if(sizeof($this->result['data']) > 0){
-                foreach ($this->result['data'] as $game) {
-                  $this->play($game['team1'],$game['team2']);
-                }
-              }
-          }
+        Log::info('Starting the Game for  ',['jobName'=>$this->game['id'],'timeStamp'=>date("Y-m-d h:i:sa")]);
+        echo "The Match ".$this->game['id']." Started between ".$this->team1['name'].' V/S '.$this->team2['name'].'<br/>';
+
+        try {
+            $now = Carbon::now();
+            $endTime = Carbon::now()->addSeconds(240);;
+            echo ($now); 
+            echo ($endTime);
+                    
+        } catch (Exception $e) {
+            echo $e->getMessage(); 
+        }
+
+
+        // unset($this->modelObj, $this->fieldObj, $this->helperObj, $this->result);
+        // $this->modelObj = new \App\Game();
+        // $this->fieldObj['conditions'] = ['id' => $this->gameId, 'status' => 1];
+        // $this->result = $this->modelObj->fetchMap($this->fieldObj);
+
+        // if($this->result['flag'] == 1) {
+        //     if(sizeof($this->result['data']) > 0) {
+        //         foreach ($this->result['data'] as $game) {  
+        //             $this->play($game['team1'],$game['team2']);
+        //         }
+        //     }
+        // }
     }
 }
